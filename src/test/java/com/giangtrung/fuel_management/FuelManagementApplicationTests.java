3083776ea8f24fed25f3234db62df0a3224720ef
B20DCCN204 <giang41245@gmail.com>
2024-07-08 17:38:35 +0700

    init project

---
+++ src/test/java/com/giangtrung/fuel_management/FuelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giangtrung.fuel_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
